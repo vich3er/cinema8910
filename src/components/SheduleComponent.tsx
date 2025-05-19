239503ef48a0a34f815cd2456d535238f4c98a17
vich3er <cherednikviktoriya@gmail.com>
2025-05-19 16:43:41 +0300

    підключила проект до FireBase та додала дані про розклад фільмів та інформацію про місця

--- src/components/SheduleComponent.tsx
+++ src/components/SheduleComponent.tsx
@@ -1,12 +1,6 @@
 import type {FC} from "react";
-import type {ISession} from "../pages/BookingPage.tsx";
+import type {ISession} from "../models/ISession.ts";
 
-// interface SheduleComponentProps {
-//     date: string;
-//     filmName: string;
-//     hall: string;
-//     seats: null[] &
-// }
 
 export const SheduleComponent:FC<ISession> = ({date, filmName, filmId, time, seats, hall}) => {
     const handleClick = () => {
